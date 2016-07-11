85ed2b4aab74db8a007219010c386a5df10a61a8
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-07-11 15:26:28 -0600

    seed-bug fix

--- seeds/1_staff.js
+++ seeds/1_staff.js
@@ -4,22 +4,18 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('staff').insert({
-          id: 1,
           first_name: 'Kyle',
           last_name: 'Coberly'
         }),
         knex('staff').insert({
-          id: 2,
           first_name: 'Elana',
           last_name: 'Kopelevich'
         }),
         knex('staff').insert({
-          id: 3,
           first_name: 'Roberto',
           last_name: 'Ortega'
         }),
         knex('staff').insert({
-          id: 4,
           first_name: 'Danny',
           last_name: 'Fritz'
         })
